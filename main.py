458b5692776303004379a1be099ce168400afe4e
tolver00 <tolversenn@gmail.com>
2025-11-19 16:40:14 +0100

    test and verbose test output

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 def add(a, b):
     return a + b
 
+def subtract(c, d):
+    return c - d
+
+
 if __name__ == "__main__":
     print(add(2, 3))
+    print(subtract(5, 7))
 
 
     
--- test_main.py
+++ test_main.py
@@ -3,4 +3,6 @@ from main import add
 def test_add():
     assert add(2, 3) == 5
 
+def test_subtract():
+    assert subtract(5, 7) == -2
 
